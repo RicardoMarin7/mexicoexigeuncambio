90d25bb17dafc4415ff87e33c3dc84057e64d135
RicardoMarin7 <Ricardo_marin97@hotmail.com>
2019-08-08 16:17:13 -0500

    Se modificó la barra de navegación para cumplir con el diseño del cliente, se modificó el nombre de la pagina así como su Logo, se modificó el color de la barra de navegación en los submenus

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,7 +7,7 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans|PT+Sans:400,700" rel="stylesheet">
     <link rel="stylesheet" href="/css/normalize.css">
     <link rel="stylesheet" href="/css/styles.css">
-    <title>México Merece Un Cambio</title>
+    <title>México Exige un Cambio</title>
     <script type="module" src="/js/all.js"></script>
     
 </head>
@@ -16,60 +16,56 @@
             <div class="contenedor">
                 <div class="barra">
                     <a href="/">
-                        <h1 class="no-margin">MéxicoMerece<span>UnCambio</span></h1>
+                        <h1 class="no-margin titulo">MéxicoExige<span>UnCambio</span></h1>
                     </a>
-                    
-                    <div class="contenedor-navegacion">
-                        <nav class="navbar navbar-expand-sm navbar-dark navegacion">
-                            <button class="navbar-toggler d-lg-none bg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"                                aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="navbar-toggler-icon custom-toggler"></span>
-                            </button>
-                            <div class="collapse navbar-collapse" id="collapsibleNavId">
-                                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                                    <li class="nav-item">
-                                            <a class="nav-link" href={{route('index')}}>Inicio<span class="sr-only">(current)</span></a>
+                    <nav class="navbar navbar-expand-sm navbar-dark navegacion">
+                        <button class="navbar-toggler d-lg-none bg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"                                aria-expanded="true" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon custom-toggler"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="collapsibleNavId">
+                            <ul class="navUL">
+                                <li class="nav-item">
+                                        <a class="nav-link" href={{route('index')}}>Inicio<span class="sr-only">(current)</span></a>
+                                </li>
+
+                                <li class="nav-item">
+                                    <a class="nav-link" href={{ route('libros')}}>Libros</a>
+                                </li>
+
+                                <li class="nav-item">
+                                    <a class="nav-link" href={{ route('messages.create')}}>Contacto</a>
+                                </li>
+                                
+                                <li class="nav-item">
+                                        <a class="nav-link" href={{route('donaciones')}}>Donaciones<span class="sr-only">(current)</span></a>
+                                </li>
+                                
+                                @if (Auth::check())
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{auth()->user()->name}}</a>
+                                        <div class="dropdown-menu {{request()->is('/') ? 'bg-none':'bg-red'}}" aria-labelledby="dropdownId">
+                                            @if (auth()->user()->hasRoles(['admin']))
+                                                <a class="dropdown-item" href={{ route('messages.index') }}>Mensajes</a>                                                     
+                                            @else
+                                                <a class="dropdown-item" href="#">Mis Compras</a>
+                                            @endif                             
+                                            <a class="dropdown-item" href={{ route('logout')}}>Cerrar Sesión</a>
+                                        </div>
+                                    </li>    
+                                @else
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Acceder</a>
+                                        <div class="dropdown-menu {{request()->is('/') ? 'bg-none':'bg-red'}}" aria-labelledby="dropdownId">
+                                            <a class="dropdown-item" href={{ route('login')}}>Iniciar Sesión</a>        
+                                            <a class="dropdown-item" href={{ route('register')}}>Registrarse</a>
+                                        </div>
                                     </li>
 
-                                    <li class="nav-item">
-                                        <a class="nav-link" href={{ route('libros')}}>Libros</a>
-                                    </li>
-
-                                    <li class="nav-item">
-                                        <a class="nav-link" href={{ route('messages.create')}}>Contacto</a>
-                                    </li>
-                                    
-                                    <li class="nav-item">
-                                            <a class="nav-link" href={{route('donaciones')}}>Donaciones<span class="sr-only">(current)</span></a>
-                                    </li>
-                                    
-                                    @if (Auth::check())
-                                        <li class="nav-item dropdown">
-                                            <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{auth()->user()->name}}</a>
-                                            <div class="dropdown-menu {{request()->is('/') ? 'bg-none':'bg-red'}}" aria-labelledby="dropdownId">
-                                                @if (auth()->user()->hasRoles(['admin']))
-                                                    <a class="dropdown-item" href={{ route('messages.index') }}>Mensajes</a>                                                     
-                                                @else
-                                                    <a class="dropdown-item" href="#">Mis Compras</a>
-                                                @endif                             
-                                                <a class="dropdown-item" href={{ route('logout')}}>Cerrar Sesión</a>
-                                            </div>
-                                        </li>    
-                                    @else
-                                        <li class="nav-item dropdown">
-                                            <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Acceder</a>
-                                            <div class="dropdown-menu {{request()->is('/') ? 'bg-none':'bg-red'}}" aria-labelledby="dropdownId">
-                                                <a class="dropdown-item" href={{ route('login')}}>Iniciar Sesión</a>        
-                                                <a class="dropdown-item" href={{ route('register')}}>Registrarse</a>
-                                            </div>
-                                        </li>
-
-                                    @endif
-                                    
-                                </ul>
-                            </div>
-                        </nav>
-                    </div>
-
+                                @endif
+                                
+                            </ul>
+                        </div>
+                    </nav>
                 </div><!--Barra-->
 
 
