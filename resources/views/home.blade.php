01487add0f12a7bd68c1faf07b1f8caa8909bd57
RicardoMarin7 <ricardo_marin97@hotmail.com>
2019-11-05 01:30:58 -0600

    Se metio a entorno de produccion, se modificó el home para hacer funcionar botones de compra

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -78,7 +78,19 @@
                     <h3>México en Cambio: Verdades en México y otras naciones</h3>
                     <p>Historia, análisis y crítica de las politicas nacionales, que por siglos se han ejercido en México, observación internacional, propuestas para volver a habilitar, reconstruir México. Es una invitación a la sociedad civil de México y el mundo para actuar proactivamente al cambio necesario de sociedad y de nacion, crear una nueva forma de ser y gobernar.</p>
                     <p class="precio">Precio inicial $500</p>
-                    <a href="#" class="boton boton-rojo d-block">Comprar Libro</a>
+                    @if (Auth::check())
+                        @if (auth()->user()->hasRoles(['admin']))
+                            <a href="download/Tomo2" class="boton boton-rojo d-block">Descargar</a>
+                        @else
+                            @if (auth()->user()->hasDonations(['Mexico en cambio Verdades en Mexico y otras naciones']))
+                                <a href="download/Tomo1" class="boton boton-rojo d-block">Descargar</a>  
+                            @else
+                                <a href="{{url('/payments/pay')}}?title=Mexico en cambio Verdades en Mexico y otras naciones&price=500" class="boton boton-rojo d-block">Comprar Libro</a>
+                            @endif                  
+                        @endif
+                    @else
+                        <a href="{{ route('login')}}" class="boton boton-rojo d-block">Comprar Libro</a>
+                    @endif
                 </div>
 
             </div>
@@ -90,9 +102,20 @@
                     <h3>Realidades Comprobadas: Una historia y dos relatos</h3>
                     <p>Este tomo consta de dos narrativas y una historia; la primera es nacional y las otras dos son vivencias internacionales. La idea es la realización de tres producciones cinematográficas para apoyar económicamente al proyecto y de esta manera cambiar los escenarios de México y el mundo.</p>
                     <p class="precio">Precio inicial $500</p>
-                    <a href="#" class="boton boton-rojo d-block">Comprar Libro</a>
+                    @if (Auth::check())
+                        @if (auth()->user()->hasRoles(['admin']))
+                            <a href="download/Tomo2" class="boton boton-rojo d-block">Descargar</a>
+                        @else
+                            @if(auth()->user()->hasDonations(['Realidades Comprobadas Una historia y dos relatos']))
+                                <a href="download/Tomo2" class="boton boton-rojo d-block">Descargar</a>
+                            @else
+                                <a href="{{url('/payments/pay')}}?title=Realidades Comprobadas Una historia y dos relatos&price=500" class="boton boton-rojo d-block">Comprar Libro</a>
+                            @endif
+                        @endif
+                    @else
+                        <a href="{{ route('login')}}" class="boton boton-rojo d-block">Comprar Libro</a>
+                    @endif
                 </div>
-
             </div>
 
             <div class="anuncio">
@@ -102,10 +125,21 @@
                     <h3>21 Versos de la verdad mexicanas, un relato y una canción desesperada</h3>
                     <p>Contiene 22 versos que se musicalizaran para presentar conciertos, con los cuales se planean beneficios económicos que se distribuirán a diversos grupos vulnerables. Dependiendo el tema de la letra, se hará una venta en forma de subasta para fomentar las fuentes de empleo para los jovenes para que de esa manera se evite la venta de drogas.</p>
                     <p class="precio">Precio inicial $500</p>
-                    <a href="#" class="boton boton-rojo d-block">Comprar Libro</a>
-                </div><!-- Contenido Anuncio-->
-            </div><!-- Anuncio-->
-        </div> <!-- contenedor anuncios-->
+                    @if (Auth::check())
+                        @if (auth()->user()->hasRoles(['admin']))
+                            <a href="download/Tomo3" class="boton boton-rojo d-block">Descargar</a>
+                        @else
+                            @if(auth()->user()->hasDonations(['21 Versos de la verdad mexicanas, un relato y una canción desesperada']))
+                                <a href="download/Tomo3" class="boton boton-rojo d-block">Descargar</a>
+                            @else
+                                <a href="{{url('/payments/pay')}}?title=21 Versos de la verdad mexicanas, un relato y una canción desesperada&price=500" class="boton boton-rojo d-block">Comprar Libro</a>
+                            @endif
+                        @endif
+                    @else
+                        <a href="{{ route('login')}}" class="boton boton-rojo d-block">Comprar Libro</a>
+                    @endif
+                </div>
+        </div>   <!-- contenedor anuncios-->
         
     </main>
 
